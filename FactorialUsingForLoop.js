8e1ea79b6072917c2fdc5b271bbc8a46f7c5de19
Aasik <aasikcs@gmail.com>
2019-06-27 04:52:39 +0530

    Create FactorialUsingForLoop.js

---
+++ FactorialUsingForLoop.js
@@ -0,0 +1,13 @@
+function Factorial(num){
+    if(num ===0 || num === 1){
+        return 1;
+    }
+
+    for(var i = num -1; i >= 1; i--){
+        num *= i;
+    }
+
+    return num;
+}
+
+alert(Factorial(5));
